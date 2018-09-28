d9b7732c9840db7a91ee49b7d71c041dc43bdd62
12520086@gm.uit.edu.vn <12520086@gm.uit.edu.vn>
2018-09-28 16:38:51 +0700

    update path

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-import LocationPermissions from "./locationPermissions";
-export default LocationPermissions;
+import { NativeModules, NativeEventEmitter } from 'react-native';
+
+const { RNLocationPermissions } = NativeModules;
+const LocationPermissionsEventEmitter = new NativeEventEmitter(
+    RNLocationPermissions
+);
+
+export default {
+    onPermissionsChange: callback => {
+        const subscription = LocationPermissionsEventEmitter.addListener(
+            RNLocationPermissions.locationPermissionsDidChange,
+            callback
+        );
+        RNLocationPermissions.startListening();
+
+        return function off() {
+            subscription.remove();
+            RNLocationPermissions.stopListening();
+        };
+    },
+    ...RNLocationPermissions
+};
